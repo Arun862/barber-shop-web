a06b509cdef4f5c1bb1c623fc196796284a1d4a4
Arun <arunk26544@gmail.com>
2025-05-27 12:07:24 +0530

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'barber-shop-web';
+
+  constructor(
+    private router: Router
+  ){}
+route(path: any){
+  console.log(path);
+  this.router.navigate(path)
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AppointmentComponent } from './pages/appointment/appointment.component';
+import { TeamComponent } from './pages/team/team.component';
+import { PriceComponent } from './pages/price/price.component';
+import { ErrorComponent } from './pages/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", 
+        component: HomeComponent
+    },
+    {
+        path: "about", 
+        component: AboutComponent
+    },
+    {
+        path: "blogs", 
+        component: BlogsComponent
+    },
+    {
+        path: "contact", 
+        component: ContactComponent
+    },
+    {
+        path: "services", 
+        component: ServicesComponent
+    },
+    {
+        path: "appointment", 
+        component: AppointmentComponent
+    },
+    {
+        path: "team", 
+        component: TeamComponent
+    },
+    {
+        path: "price", 
+        component: PriceComponent
+    },
+    {
+        path: "error", 
+        component: ErrorComponent
+    },
+];
