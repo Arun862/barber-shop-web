b8d0cf13c9023af01e2a124dd4da40e69c9b919f
Arun <arunk26544@gmail.com>
2025-05-28 16:32:12 +0530

    loop

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,51 @@ import { Router, RouterModule } from '@angular/router';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeComponent {
+
+
+    sponsors= [
+     {
+      image: "sponsor-1.png"
+    },
+
+    {
+      image: "sponsor-2.png"
+    },
+
+    {
+      image: "sponsor-3.png"
+    }, 
+
+    {
+     image: "sponsor-4.png"
+    },
+
+  {
+    image: "sponsor-5.png"
+  },
+
+    {
+      image: "sponsor-1.png"
+    },
+
+    {
+      image: "sponsor-2.png"
+    },
+
+    {
+      image: "sponsor-3.png"
+    }, 
+
+    {
+     image: "sponsor-4.png"
+    },
+
+  {
+    image: "sponsor-5.png"
+  }
+];
+
+
 constructor(
     private router: Router
   ){}
